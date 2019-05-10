e1172ffb16016cce95262cd57957a30db861d26f
wangchangyan <1451328618@qq.com>
2019-05-10 10:49:50 +0800

    更新

---
+++ src/components/common/footer.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="footer">
+        <div class="min-footer">
+            <div class="div1">
+                <div class="pic1"></div>
+                <div class="pic2"></div>
+            </div>
+            <div class="div2">
+                <p class="p1">产品与服务</p>
+                <p class="p2">设备接入平台</p>
+                <p class="p2">场景应用平台</p>
+                <p class="p2">开发平台</p>
+                <p class="p2">数据应用平台</p>               
+            </div>
+            <div class="div3">
+                <p class="p1">解决方案</p>
+                <p class="p2">智慧楼宇解决方案</p>
+                <p class="p2">智慧园区解决方案</p>                  
+                <p class="p2">智慧场馆解决方案</p>
+                <p class="p2">智慧地产解决方案</p>
+                <p class="p2">智慧商业解决方案</p>
+                <p class="p2">智慧城市解决方案</p>             
+            </div>
+            <div class="div4">
+                <p class="p1">关于我们</p>
+                <p class="p2">关于博彦科技</p>           
+            </div>
+            <div class="div5">
+                <p class="p1">咨询电话</p>
+                <p class="p2">400 000 6282</p>                                 
+            </div>
+        </div>
+        <div class="footer-bottom">@2019 博彦科技 博彦物联   用户协议   隐私权政策   服务状态 </div>           
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+      .footer{
+            height: 328px;
+            background-color:#363636;
+            position: relative;
+            .footer-bottom{
+                width: 100%;
+                height: 38px;
+                line-height:38px;
+                color:#939793;
+                text-align: center;
+                position: absolute;;
+                bottom:0;
+                left:0;
+                background-color: #252525;
+            }
+            .min-footer{
+                width: 1200px;
+                margin:0 auto;                
+                .div1{
+                    float: left;
+                    width: 88px;
+                    margin-right:150px;
+                    padding-top:12px;
+                    .pic1,.pic2{
+                        width:88px;
+                        height: 85px;
+                        margin:30px 0;
+                        background-color: #4dafde;
+                    }
+                }
+                .div2,.div3,.div4,.div5{
+                    .p1{
+                        color:#fff;
+                        font-size:14px;
+                        margin-bottom:15px;
+                    }
+                    .p2{
+                        margin-bottom:15px;
+                        color:#939793
+                    }
+                }
+                .div2{
+                    float: left;
+                    padding-top:50px;
+                    width: 120px;
+                    margin-right: 140px;
+                  
+                }
+                .div3{
+                    float: left;
+                    padding-top:50px;
+                    width: 120px;
+                    margin-right: 140px;
+                }
+                .div4{
+                    float: left;
+                    padding-top:50px;
+                    width: 120px;
+                    margin-right: 140px;
+                }
+                .div5{
+                    float: left;
+                    padding-top:50px;
+                    width: 120px;
+                }
+            }
+          
+        }
+</style>
+
+
--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -158,43 +158,8 @@
                  </div>                
             </div>
         </div>
-        <div class="footer">
-            <div class="min-footer">
-                 <div class="div1">
-                    <div class="pic1"></div>
-                    <div class="pic2"></div>
-                </div>
-                <div class="div2">
-                    <p class="p1">产品与服务</p>
-                    <p class="p2">设备接入平台</p>
-                    <p class="p2">场景应用平台</p>
-                    <p class="p2">开发平台</p>
-                    <p class="p2">数据应用平台</p>
-                
-                </div>
-                <div class="div3">
-                    <p class="p1">解决方案</p>
-                    <p class="p2">智慧楼宇解决方案</p>
-                    <p class="p2">智慧园区解决方案</p>                  
-                    <p class="p2">智慧场馆解决方案</p>
-                    <p class="p2">智慧地产解决方案</p>
-                    <p class="p2">智慧商业解决方案</p>
-                    <p class="p2">智慧城市解决方案</p>             
-                </div>
-                  <div class="div4">
-                    <p class="p1">关于我们</p>
-                    <p class="p2">关于博彦科技</p>           
-                </div>
-                  <div class="div5">
-                    <p class="p1">咨询电话</p>
-                    <p class="p2">400 000 6282</p>                                 
-                </div>
-            </div>
-            <div class="footer-bottom">@2019 博彦科技 博彦物联   用户协议   隐私权政策   服务状态 </div>
-           
-        </div>
-        <div class="floating">
-            
+        <footer-bar></footer-bar>
+        <div class="floating">            
             <span>意见 · 反馈</span>
             <i class="i"></i>
             <div class="float-window">
@@ -215,7 +180,11 @@
     </div>
 </template>
 <script>
+import footerBar from '../common/footer'
 export default {
+    components:{
+        footerBar
+    },
     data(){
         return{
             scrollTag:false
@@ -249,29 +218,33 @@ export default {
 }
 </script>
 <style lang="less">
-.el-carousel__container{
-    width:100%;
-    height: 649px;
-    // z-index: 2;
+.home_page{
+    .el-carousel__arrow i {
+        cursor: pointer;
+        font-size: 40px;
+    }
+    .el-carousel__container{
+        width:100%;
+        height: 540px;
+        // z-index: 2;
+    }
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 18px;
+        height: 540px;
+        background: url(../../assets/images/banner.png) no-repeat center center;
+        background-size:1920px 540px;
+        margin: 0;
+        text-align:center;
+    }
+    .el-carousel__button{
+        height:3px;
+    }
+    .el-carousel__indicators--horizontal{
+        bottom:25px;
+    }
 }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    height: 649px;
-    background: url(../../assets/images/banner_01.png) no-repeat center center;
-    background-size:1920px 649px;
-    margin: 0;
-    text-align:center;
-  }
-  
-  .home_page{
-        .el-carousel__button{
-            height:3px;
-        }
-        .el-carousel__indicators--horizontal{
-            bottom:25px;
-        }
-  }
+
 </style>
 <style lang="less" scoped>
     .home_page{
@@ -331,7 +304,7 @@ export default {
         }
         .swiper{
             width:100%;
-            height:649px;
+            height:540px;
             position: relative;
         }
         .info1{
@@ -662,74 +635,7 @@ export default {
                  
             }
         }
-        .footer{
-            height: 328px;
-            background-color:#363636;
-            position: relative;
-            .footer-bottom{
-                width: 100%;
-                height: 38px;
-                line-height:38px;
-                color:#939793;
-                text-align: center;
-                position: absolute;;
-                bottom:0;
-                left:0;
-                background-color: #252525;
-            }
-            .min-footer{
-                width: 1200px;
-                margin:0 auto;                
-                .div1{
-                    float: left;
-                    width: 88px;
-                    margin-right:150px;
-                    padding-top:12px;
-                    .pic1,.pic2{
-                        width:88px;
-                        height: 85px;
-                        margin:30px 0;
-                        background-color: #4dafde;
-                    }
-                }
-                .div2,.div3,.div4,.div5{
-                    .p1{
-                        color:#fff;
-                        font-size:14px;
-                        margin-bottom:15px;
-                    }
-                    .p2{
-                        margin-bottom:15px;
-                        color:#939793
-                    }
-                }
-                .div2{
-                    float: left;
-                    padding-top:50px;
-                    width: 120px;
-                    margin-right: 140px;
-                  
-                }
-                .div3{
-                    float: left;
-                    padding-top:50px;
-                    width: 120px;
-                    margin-right: 140px;
-                }
-                .div4{
-                    float: left;
-                    padding-top:50px;
-                    width: 120px;
-                    margin-right: 140px;
-                }
-                .div5{
-                    float: left;
-                    padding-top:50px;
-                    width: 120px;
-                }
-            }
-          
-        }
+      
         .floating{
             width: 35px;
             height: 115px;         
