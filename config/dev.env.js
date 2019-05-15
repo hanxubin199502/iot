ab86ec09d4d9bcba7ee668ba8ff022a2ef708b1d
limingda <limingda@beyongsoft.com>
2019-05-15 11:29:42 +0800

    官网1.0

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API_ROOT: '"192.168.7.152:8970"',    
+    API_ROOT: '"192.168.7.152:8970"',   //dev
+    // API_ROOT: '"192.168.14.127:8970"',   
+
  
 })
