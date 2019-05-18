8c9b49e022a58d74c211188ae4a25be2f8922316
wangchangyan <1451328618@qq.com>
2019-05-18 17:40:17 +0800

    改二维码

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -3,7 +3,7 @@
         <div class="min-footer">
             <div class="div1">
                 <div class="pic1"><img src="https://www.beyondsoft.com/images/erweima_50.png" title="公众号关注二维码"></div>
-                <div class="pic2"><img src="../../assets/images/er1.png" title="APP下载二维码"></div>
+                <div class="pic2"><img src="../../assets/images/er2.png" title="APP下载二维码"></div>
             </div>
             <div class="div2">
                 <p class="p1">产品与服务</p>
