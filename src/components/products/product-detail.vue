4f6a6ef3075f5455469f01f94bf1305c7dab4c4e
limingda <limingda@beyongsoft.com>
2019-05-28 18:34:24 +0800

    解决方案 、产品、合作、开发者社区、头部底部链接跳转、部分细化

---
+++ src/components/products/product-detail.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="product_detail">
+    <headerBar/>
+    <div
+      class="header_background"
+      :style="'background:url('+productList.imagesPath+') center center'"
+    >
+      <div class="product_title center clearfix">
+        <div class="product_name">{{productList.productName}}</div>
+        <div class="product_brief">{{productList.productAbs}}</div>
+      </div>
+    </div>
+    <div class="content center">
+      <div class="product_content_name">{{productList.productName}}</div>
+      <div class="product_content" v-html="productList.productContext"></div>
+    </div>
+    <footerBar/>
+  </div>
+</template>
+
+
+<script>
+import footerBar from "../common/footer";
+import headerBar from "../common/header";
+export default {
+  components: {
+    footerBar,
+    headerBar
+  },
+  data() {
+    return {
+      userInfo: {},
+      productList: [],
+      id: ''
+    };
+  },
+  created() {
+    this.getProductDetail();
+    let url = location.href;
+    if (url.indexOf("bysiot") > 0) {
+      document.domain = "bysiot.com";
+      this.userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
+    }
+  },
+  watch: {
+    $route() {
+      this.id = this.$route.params.id;
+    },
+    id() {
+      this.getProductDetail();
+    }
+  },
+  mounted() {
+    // this.routerUrl = location.hash
+  },
+  beforeDestroy() {
+    // window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  methods: {
+    //获取产品详情
+    getProductDetail() {
+    //   console.log("hahahahaha");
+      let params = {
+        productContextId: this.$route.params.id
+      };
+      this.$http
+        .get(
+          this.$api.getApiAddress(
+            "operationplatformmgn/o/saas/platform-productContext/query_productContext",
+            "API_ROOT"
+          ),
+          params
+        )
+        .then(res => {
+          this.productList = res.data;
+          // console.log(this.solutionList)
+        });
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.product_detail {
+  .header_background {
+    // position: relative;
+    width: 100%;
+    height: 560px;
+    // background: url(../../assets/images/banner-bgi-product.png) center center;
+    .product_title {
+      position: relative;
+      color: #ffffff;
+      .product_name {
+        position: absolute;
+        top: 170px;
+        left: 0;
+        font-size: 44px;
+      }
+      .product_brief {
+        position: absolute;
+        width: 600px;
+        top: 255px;
+        font-size: 18px;
+      }
+    }
+  }
+  .content {
+    padding: 15px 0;
+    color: #3f3f3f;
+    .product_content_name {
+      margin-top: 50px;
+      width: 100%;
+      text-align: center;
+      font-size: 30px;
+    }
+    .product_content {
+      margin-top: 35px;
+      font-size: 20px;
+    }
+  }
+}
+</style>
