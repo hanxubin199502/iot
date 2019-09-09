93d96c460f361846851f64b4e620dc8eeeee3aa0
wangchangyan <1451328618@qq.com>
2019-09-09 15:43:08 +0800

    bug fix

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,8 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API_ROOT: '"192.168.7.152:8970"',   //dev
-    // API_ROOT: '"192.168.14.127:8970"',   
-
- 
+    API_ROOT: '"192.168.7.160:8970"',   //dev
+     
 })
