cbb0260bd3586454729e41dd1ea3c0b871ec4f14
guoziyu <1614547721@qq.com>
2020-07-01 17:35:18 +0800

    增加集智生活解决方案页面

---
+++ src/components/solutions/jizhi-commerce.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="solution_detail">
+    <headerBar />
+    <div class="jizhi-con"></div>
+    <footerBar />
+  </div>
+</template>
+
+<script>
+import footerBar from "../common/footer";
+import headerBar from "../common/header";
+export default {
+  components: {
+    footerBar,
+    headerBar
+  }
+};
+</script>
+<style lang="less" scoped>
+.solution_detail {
+  .jizhi-con {
+    height: 5914px;
+    background: url('../../assets/images/jizhishenghuo.jpg') no-repeat center;
+  }
+}
+</style>
