db334e6c77f76ee1ea980cd9ac13ec23f0e519fd
t-hxb <t-hxb@beyondsoft.com>
2020-07-22 16:35:53 +0800

    还是bug

--- src/components/newcenterlist/newsCenter.vue
+++ src/components/newcenterlist/newsCenter.vue
@@ -531,6 +531,7 @@ export default {
                 color: #999;
                 font-size: 14px;
                 text-align: left;
+                margin-top: 10px;
               }
             }
           }
